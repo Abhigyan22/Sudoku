0e3c92b0de03cc051e2c5e4373edc601a26ea0dd
Abhigyan Kaushik <abhigyankaushik22@gmail.com>
2022-01-20 13:50:41 +0530

    Add the grid (png)

--- game.py
+++ game.py
@@ -1,3 +1,25 @@
+import pygame
 from sudoku import Sudoku
-puzzle = Sudoku(3).difficulty(0.5)
-puzzle.show()
+from sys import exit
+pygame.init()
+#Initializing the module
+
+WIDTH = 504
+HEIGHT = 504
+
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+#Screen of the game
+IMG = pygame.image.load('grid.png')
+#Grid of the game
+
+running = True
+
+while running:
+    for event in pygame.event.get(): #Event loop
+        if event.type==pygame.QUIT: #If clicked on close
+            running=False
+    SCREEN.blit(IMG, (0,0))
+    pygame.display.update() #Updates the screen
+    pygame.display.flip()
+pygame.quit()
+exit()
