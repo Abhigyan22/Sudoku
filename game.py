e2f1c55090a9cd7cee2ed9048244db1034ebbbf8
Abhigyan Kaushik <abhigyankaushik22@gmail.com>
2022-01-22 14:58:56 +0530

    Feat: Implemented the sudoku module to display actual game

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+from random import randrange
 import pygame
 from cells import *
 from sudoku import Sudoku
 from sys import exit
+
 pygame.init()
 #Initializing the module
 
@@ -12,14 +14,29 @@ SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 #Screen of the game
 pygame.display.set_caption("Sudoku") #Sets the title of the window
 
-LOGO = pygame.image.load('sudokuLogo.png')
+LOGO = pygame.image.load('IMG/sudokuLogo.png')
 pygame.display.set_icon(LOGO) #Set the logo of the game
 
+font = pygame.font.Font('FONT/baars.ttf', 50)
+#Custom font
+
+def create_board():
+    puzzle =  Sudoku(3).difficulty(0.5)
+    board = puzzle.board
+    current_position = 0
+    for row in board:
+        for num in row:
+            cells[current_position].number = num
+            current_position+=1
+
 def redraw_window():
-    """Draws the parts of the window everyframe"""
+    """Draws the parts of the window every frame"""
     SCREEN.fill((255,255,255)) #Fills the screen with white (so that there is no black border)
     for cell in cells: 
-        pygame.draw.rect(SCREEN, cell.color, pygame.Rect(cell.x+1,cell.y+1,cell.distance-1,cell.distance-1))
+        pygame.draw.rect(SCREEN, cell.color, pygame.Rect(cell.x,cell.y,cell.distance,cell.distance))
+        if cell.number: #I.e it isnt a falsy value
+            text = font.render(str(cell.number), True, (000,000,000))
+            SCREEN.blit(text, (cell.x+18, cell.y))
         #Draws a rect with color of the cell
     make_grid() # Makes the grid
      
@@ -32,14 +49,14 @@ def make_grid():
     for line in range(1,9):
         if line%3==0:
             pygame.draw.line(SCREEN, (000,000,000), (current_place, 0), (current_place, WIDTH), 3) #Horizontal
-            pygame.draw.line(SCREEN, (000,000,000), (0, current_place), (WIDTH, current_place), 3) #Vertical
+            pygame.draw.line(SCREEN, (000,000,000), (0, current_place), (HEIGHT, current_place), 3) #Vertical
             current_place+=distance
         else:
-            pygame.draw.line(SCREEN, (000,000,000), (current_place, 0), (current_place, HEIGHT)) #Horizontal
-            pygame.draw.line(SCREEN, (000,000,000), (0, current_place), (WIDTH, current_place)) #Vertical
+            pygame.draw.line(SCREEN, (000,000,000), (current_place, 0), (current_place, WIDTH)) #Horizontal
+            pygame.draw.line(SCREEN, (000,000,000), (0, current_place), (HEIGHT, current_place)) #Vertical
             current_place+=distance
 
-def main():
+def game():
     """The main part of the game"""
     running = True
 
@@ -49,10 +66,11 @@ def main():
         for event in pygame.event.get(): #Event loop
             if event.type==pygame.QUIT: #If clicked on close
                 running=False
-            if event.type ==pygame.MOUSEBUTTONDOWN:
+            if event.type ==pygame.MOUSEBUTTONDOWN and event.button==1:
+                                                        #Left mouse button
                 for cell in cells:
                     if cell.is_over(pos):
-                        cell.color = (255,0,0)
+                        cell.color = (0,200,0)
                     else:
                         cell.color = (255,255,255)
 
@@ -60,4 +78,5 @@ def main():
         pygame.display.flip()
     pygame.quit()
     exit()
-main()
+create_board()
+game()
